57fdd7598785d475f37fe6d38e9d1085248308fe
Jessica Kerr <jessitron@atomist.com>
2018-06-12 12:35:46 -0500

    might work

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { SpringSupport } from "./spring";
 export { LocalExecutableJarDeployer } from "./support/spring/deploy/localSpringBootDeployers";
-export { IsJava } from "./support/java/pushTests";
+export { IsJava, MaterialChangeToJavaRepo } from "./support/java/pushTests";
 export { IsMaven } from "./support/maven/pushTests";
 export { HasSpringBootApplicationClass, HasSpringBootPom } from "./support/spring/pushTests";
 export { springBootTagger } from "./support/spring/springTagger";
@@ -11,3 +11,8 @@ export { AllJavaFiles, JavaAndKotlinSource } from "./support/java/javaProjectUti
 export { MavenFingerprinter } from "./support/maven/fingerprint/MavenFingerprinter"
 export { ImportDotStarCategory, ImportDotStarReviewer } from "./support/java/review/importDotStarReviewer";
 export { ImportFileIoCategory, FileIoImportReviewer } from "./support/java/review/fileIoImportReviewer"
+export { HardCodedPropertyReviewer, HardcodePropertyCategory } from "./support/spring/review/hardcodedPropertyReviewer";
+export { MavenBuilder } from "./support/maven/build/MavenBuilder"
+export { CommonJavaGeneratorConfig } from "./support/java/generate/generatorConfig"
+export { springBootGenerator } from "./support/spring/generate/springBootGenerator";
+export { ProvidedDependencyReviewer } from "./support/maven/review/providedDependencyReviewer"
