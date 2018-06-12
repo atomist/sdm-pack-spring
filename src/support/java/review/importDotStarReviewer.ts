cb1615be75e915bce75aeb47ad5289ed778ab429
Jessica Kerr <jessitron@atomist.com>
2018-06-12 12:08:20 -0500

    Remove espower because it makes a src dir in dist
    and I don't want a src dir in dist, in confuses intellij even more on imports when linked

---
+++ src/support/java/review/importDotStarReviewer.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright © 2018 Atomist, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { ReviewerRegistration } from "@atomist/sdm";
+import { patternMatchReviewer } from "@atomist/sdm/code/review/patternMatchReviewer";
+import { JavaAndKotlinSource } from "./Globs";
+
+// JESS: Moved from sample-sdm to sdm-pack-spring
+
+export const ImportDotStarCategory = "Lazy import";
+
+export const ImportDotStarReviewer: ReviewerRegistration = patternMatchReviewer(
+    ImportDotStarCategory,
+    {globPattern: JavaAndKotlinSource, severity: "info"},
+    {
+        name: ImportDotStarCategory,
+        antiPattern: /import .*\.\*/,
+        comment: "Don't import .*. Import types individually",
+    },
+);
