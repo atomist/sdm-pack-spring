c5259ecca2dbae3c1c30be66c8ac0e2cb84db5d4
Christian Dupuis <cd@atomist.com>
2018-09-06 15:31:16 +0200

    Add MavenProgressReporter
    [changelog:added]

--- index.ts
+++ index.ts
@@ -97,5 +97,7 @@ export {
     springBootTagger,
 } from "./lib/spring/springTagger";
 
+export * from "./lib/maven/MavenProgressReporter"
+
 export * from "./lib/maven/inspection/findDependenciesCommand";
 export * from "./lib/maven/inspection/findDependencies";
