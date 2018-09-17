8ca42ecf79b41ebab54e55562ac1f01dc438937a
Rod Johnson <rod@atomist.com>
2018-09-17 15:30:16 +1000

    Add properties parser

---
+++ lib/properties/propertiesParser.ts
@@ -0,0 +1,110 @@
+import { Project } from "@atomist/automation-client";
+import { Microgrammar, Updatable } from "@atomist/microgrammar";
+import { CodeTransform } from "@atomist/sdm";
+
+/**
+ * File of updated properties. Change keys or values and call flush() to write out any changes.
+ */
+export interface PropertiesFile {
+
+    path: string;
+
+    /**
+     * Updatable properties
+     */
+    properties: Property[];
+
+    /**
+     * Add a property to the end of the file
+     * @param {Property} prop
+     */
+    addProperty(prop: Property): Promise<void>;
+
+    /**
+     * Flush changes
+     * @return {Promise<void>}
+     */
+    flush(): Promise<void>;
+}
+
+/**
+ * Properties are updatable
+ */
+export interface Property {
+    key: string;
+    value: string;
+}
+
+/**
+ * Parse the given file within the project, returning updatable properties
+ * @param {Project} p
+ * @param {string} path
+ * @return {Promise<PropertiesFile>}
+ */
+export async function parseProperties(p: Project, path: string): Promise<PropertiesFile> {
+    const f = await p.getFile(path);
+    if (!!f) {
+        const content = await f.getContent();
+        const updatable = Microgrammar.updatable<Property>(propertiesGrammar.findMatches(content), content);
+        return new PropertiesFileImpl(path, updatable, p);
+    } else {
+        return new PropertiesFileImpl(path, undefined, p);
+    }
+}
+
+/**
+ * Code transform to add the property to the given file,
+ * creating it if it doesn't exist. Updates any existing property.
+ * @param {Property} prop
+ * @param {string} path
+ * @return {CodeTransform}
+ */
+export function addProperty(prop: Property, path: string): CodeTransform {
+    return async p => {
+        const propsFile = await parseProperties(p, path);
+        return propsFile.addProperty(prop);
+    };
+}
+
+class PropertiesFileImpl implements PropertiesFile {
+
+    get properties() {
+        return !!this.updatable ? this.updatable.matches : [];
+    }
+
+    public async addProperty(prop: Property) {
+        const existing = this.properties.find(p => p.key === prop.key);
+        if (!!existing) {
+            existing.value = prop.value;
+            return this.flush();
+        }
+        const f = await this.project.getFile(this.path);
+        if (!!f) {
+            const oldContent = await f.getContent();
+            await f.setContent(oldContent + `\n\n${prop.key}=${prop.value}\n`);
+        } else {
+            await this.project.addFile(this.path, `\n${prop.key}=${prop.value}\n`);
+        }
+    }
+
+    public async flush() {
+        if (!!this.updatable) {
+            const f = await this.project.getFile(this.path);
+            await f.setContent(this.updatable.updated());
+        }
+    }
+
+    constructor(public readonly path: string,
+                private readonly updatable: Updatable<Property> | undefined,
+                private readonly project: Project) {
+    }
+}
+
+const PropertyKey = /[^[=\s]+/;
+
+const propertiesGrammar = Microgrammar.fromString<Property>(
+    "${key}=${value}",
+    {
+        key: PropertyKey,
+        value: /.*/,
+    });
---
+++ test/properties/propertiesParser.test.ts
@@ -0,0 +1,129 @@
+import { InMemoryProject } from "@atomist/automation-client";
+import { InMemoryProjectFile } from "@atomist/sdm";
+import * as assert from "assert";
+import { addProperty, parseProperties } from "../../lib/properties/propertiesParser";
+
+describe("propertyHandling", () => {
+
+    describe("parseProperties", () => {
+
+        it("returns no properties when no file", async () => {
+            const p = InMemoryProject.of();
+            const props = await parseProperties(p, "not.there.properties");
+            assert.strictEqual(props.properties.length, 0);
+        });
+
+        it("adds file when no properties", async () => {
+            const path = "thing.properties";
+            const p = InMemoryProject.of();
+            const props = await parseProperties(p, path);
+            assert.strictEqual(props.properties.length, 0);
+            await props.addProperty({ key: "foo", value: "bar" });
+            assert.strictEqual(p.findFileSync(path).getContentSync(),
+                "\nfoo=bar\n");
+        });
+
+        it("finds single property", async () => {
+            const p = InMemoryProject.of(new InMemoryProjectFile("thing.properties", "foo=bar"));
+            const props = await parseProperties(p, "thing.properties");
+            assert.strictEqual(props.properties.length, 1);
+            assert.strictEqual(props.properties[0].key, "foo");
+            assert.strictEqual(props.properties[0].value, "bar");
+        });
+
+        it("finds single property, ignoring blank lines and comment", async () => {
+            const p = InMemoryProject.of(new InMemoryProjectFile("thing.properties", OneWithComments));
+            const props = await parseProperties(p, "thing.properties");
+            assert.strictEqual(props.properties.length, 1);
+            assert.strictEqual(props.properties[0].key, "foo");
+            assert.strictEqual(props.properties[0].value, "bar");
+        });
+
+        it("finds two properties, ignoring blank lines and comment", async () => {
+            const p = InMemoryProject.of(new InMemoryProjectFile("thing.properties", TwoWithComments));
+            const props = await parseProperties(p, "thing.properties");
+            assert.strictEqual(props.properties.length, 2);
+            assert.strictEqual(props.properties[0].key, "foo");
+            assert.strictEqual(props.properties[0].value, "bar");
+            assert.strictEqual(props.properties[1].key, "a.b.c");
+            assert.strictEqual(props.properties[1].value, "20394029384029834$$$$abc");
+        });
+
+        it("changes property, ignoring blank lines and comment", async () => {
+            const p = InMemoryProject.of(new InMemoryProjectFile("thing.properties", TwoWithComments));
+            const props = await parseProperties(p, "thing.properties");
+            props.properties[1].value = "thing";
+            await props.flush();
+            assert.strictEqual(p.findFileSync("thing.properties").getContentSync(),
+                TwoWithComments.replace("20394029384029834$$$$abc", "thing"));
+        });
+
+        it("adds property to existing properties", async () => {
+            const p = InMemoryProject.of(new InMemoryProjectFile("thing.properties", TwoWithComments));
+            const props = await parseProperties(p, "thing.properties");
+            await props.addProperty({ key: "x", value: "y" });
+            assert.strictEqual(p.findFileSync("thing.properties").getContentSync(),
+                TwoWithComments + `\n\nx=y\n`);
+        });
+
+        it("does not add property to existing properties when it's already there", async () => {
+            const p = InMemoryProject.of(new InMemoryProjectFile("thing.properties", TwoWithComments));
+            const props = await parseProperties(p, "thing.properties");
+            await props.addProperty({ key: "foo", value: "bar" });
+            assert.strictEqual(p.findFileSync("thing.properties").getContentSync(),
+                TwoWithComments);
+        });
+
+        it("changes property in existing properties when it's already there", async () => {
+            const p = InMemoryProject.of(new InMemoryProjectFile("thing.properties", TwoWithComments));
+            const props = await parseProperties(p, "thing.properties");
+            await props.addProperty({ key: "foo", value: "baz" });
+            assert.strictEqual(p.findFileSync("thing.properties").getContentSync(),
+                TwoWithComments.replace("bar", "baz"));
+        });
+
+    });
+
+    describe("addProperty CodeTransform", () => {
+
+        it("changes property, ignoring blank lines and comment", async () => {
+            const p = InMemoryProject.of(new InMemoryProjectFile("thing.properties", TwoWithComments));
+            await addProperty({ key: "a.b.c", value: "thing" }, "thing.properties")(p, undefined);
+            assert.strictEqual(p.findFileSync("thing.properties").getContentSync(),
+                TwoWithComments.replace("20394029384029834$$$$abc", "thing"));
+        });
+
+        it("adds file when no properties", async () => {
+            const path = "thing.properties";
+            const p = InMemoryProject.of();
+            await addProperty({ key: "foo", value: "bar" }, path)(p, undefined);
+            assert.strictEqual(p.findFileSync(path).getContentSync(),
+                "\nfoo=bar\n");
+        });
+
+        it("adds property to existing properties", async () => {
+            const p = InMemoryProject.of(new InMemoryProjectFile("thing.properties", TwoWithComments));
+            await addProperty({ key: "x", value: "y" }, "thing.properties")(p, undefined);
+            assert.strictEqual(p.findFileSync("thing.properties").getContentSync(),
+                TwoWithComments + `\n\nx=y\n`);
+        });
+    });
+
+});
+
+const OneWithComments = `
+# These are my properties
+
+foo=bar`;
+
+const TwoWithComments = `
+# These are my properties
+
+foo=bar
+
+# And another
+# comment
+
+a.b.c=20394029384029834$$$$abc
+
+`;
