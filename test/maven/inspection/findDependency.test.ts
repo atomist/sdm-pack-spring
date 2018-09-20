d756de767952fb77551f4dc83ceafa5c3da136d5
lievendoclo <lieven@atomist.com>
2018-09-20 12:51:34 +0200

    Make fingerprint test more stable

--- test/maven/inspection/findDependency.test.ts
+++ test/maven/inspection/findDependency.test.ts
@@ -44,12 +44,12 @@ describe("FindDependency", () => {
 
 });
 
-export function tempProject(id?: RepoId): LocalProject {
+function tempProject(id?: RepoId): LocalProject {
     const dir = tmp.dirSync();
     return new NodeFsLocalProject(id || "temp", dir.name);
 }
 
-export function testPom() {
+function testPom() {
     return `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
 	xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
