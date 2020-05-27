af47bff04b96b69d7bbdfcd6e2dd62c175a7999b
ipcrm <mcadorette@gmail.com>
2020-05-26 22:07:26 -0400

    Remove deployment references
    The deployer logic has been removed from 1.9x versions of sdm-core

--- test/java/transform/bringInFile.test.ts
+++ test/java/transform/bringInFile.test.ts
@@ -23,10 +23,12 @@ describe("bringInFile", () => {
     it("should bring in file to empty project", async () => {
         const p = InMemoryProject.of();
         const trans = bringInFile(
-            "https://raw.githubusercontent.com/spring-guides/gs-securing-web/master/complete/src/main/java/hello/WebSecurityConfig.java",
-            "src/main/java");
+          // tslint:disable-next-line: max-line-length
+          "https://raw.githubusercontent.com/spring-guides/gs-securing-web/master/complete/src/main/java/com/example/securingweb/WebSecurityConfig.java",
+          "src/main/java",
+        );
         await trans(p, undefined);
-        const path = "src/main/java/hello/WebSecurityConfig.java";
+        const path = "src/main/java/com/example/securingweb/WebSecurityConfig.java";
         const f = p.findFileSync(path);
         assert(!!f, `Found ${await p.totalFileCount()} files`);
     });
