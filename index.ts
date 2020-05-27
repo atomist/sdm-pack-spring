d776b207b5f15773efb7d7bef58491e24e917bfc
ipcrm <mcadorette@gmail.com>
2020-05-27 02:09:22 +0000

    Autofix: TypeScript header
    [atomist:generated] [atomist:autofix=typescript_header]

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 /*
- * Copyright © 2019 Atomist, Inc.
+ * Copyright © 2020 Atomist, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- s* you may not use this file except in compliance with the License.
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *     http://www.apache.org/licenses/LICENSE-2.0
